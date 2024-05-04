e7ead58f85e186998afaaf0c5c3ee68a94d360d4
Sneha <130309560+SnehaSajeev@users.noreply.github.com>
2024-05-04 16:14:45 +0530

    push

--- lib/ui/screens/splash/splash_screen.dart
+++ lib/ui/screens/splash/splash_screen.dart
@@ -1,8 +1,10 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../../../core/provider/auth/auth_provider.dart';
+import '../../utils/constants/image_assets.dart';
 
 @RoutePage()
 class SplashScreen extends StatelessWidget {
@@ -12,13 +14,13 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     context.read<AuthProvider>().start();
 
-    return const Scaffold(
+    return Scaffold(
       body: Padding(
-        padding: EdgeInsets.all(20),
+        padding: const EdgeInsets.all(20),
         child: Column(
           children: [
-            // Image.asset(name)
-            Expanded(
+            Image.asset(ImageAssets.logo),
+            const Expanded(
               child: Center(
                 child: Text(
                   'My Helper',
@@ -26,7 +28,7 @@ class SplashScreen extends StatelessWidget {
                 ),
               ),
             ),
-            Text(
+            const Text(
               'Powered By Students of\nJaypee University of Guna MP',
               style: TextStyle(fontSize: 12),
               textAlign: TextAlign.center,
---
+++ lib/ui/utils/constants/image_assets.dart
@@ -0,0 +1,3 @@
+abstract class ImageAssets{
+  static const String logo = 'assets/images/logo.png';
+}
