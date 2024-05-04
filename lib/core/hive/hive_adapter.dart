b07f47a87235997b60d0752501c1eaa84e81a520
afl-h <58439230+afl-h@users.noreply.github.com>
2024-05-04 23:30:52 +0530

    Update hive_adapter.dart

--- lib/core/hive/hive_adapter.dart
+++ lib/core/hive/hive_adapter.dart
@@ -3,10 +3,13 @@ import 'package:hive/hive.dart';
 import '../model/settings/settings_model.dart';
 import '../model/user/user_model.dart';
 
-
 abstract class HiveAdapter {
   static void register() {
-    Hive.registerAdapter(SettingsAdapter());
-    Hive.registerAdapter(UserAdapter());
+    if (!Hive.isAdapterRegistered(SettingsAdapter().typeId)) {
+      Hive.registerAdapter(SettingsAdapter());
+    }
+    if (!Hive.isAdapterRegistered(UserAdapter().typeId)) {
+      Hive.registerAdapter(UserAdapter());
+    }
   }
 }
